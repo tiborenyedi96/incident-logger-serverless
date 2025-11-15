6abd5fc4004aec592f2712d7a8a82fa7fcef50ff
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-15 23:15:15 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,17 @@
 module "vpc" {
   source = "./modules/vpc"
+  name   = "incident-logger"
+}
+
+module "rds" {
+  source      = "./modules/rds"
+  name        = "incident-logger"
+  db_username = "appuser"
+
+  subnet_ids = [
+    module.vpc.private_a_subnet_id,
+    module.vpc.private_b_subnet_id
+  ]
+
+  vpc_id = module.vpc.vpc_id
 }
