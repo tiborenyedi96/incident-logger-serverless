86a1cd5fee2d63716ea5d6c40801e99228e43799
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-16 20:42:27 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,3 +15,18 @@ module "rds" {
 
   vpc_id = module.vpc.vpc_id
 }
+
+module "rds_proxy" {
+  source          = "./modules/rds_proxy"
+  name            = "incident-logger"
+  vpc_id          = module.vpc.vpc_id
+  rds_cluster_identifier = module.rds.rds_cluster_identifier
+
+  subnet_ids = [
+    module.vpc.private_a_subnet_id,
+    module.vpc.private_b_subnet_id
+  ]
+
+  rds_cluster_arn               = module.rds.rds_cluster_arn
+  rds_secretsmanager_secret_arn = module.rds.rds_secretsmanager_secret_arn
+}
