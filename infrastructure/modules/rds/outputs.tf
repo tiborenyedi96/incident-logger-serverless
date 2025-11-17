a141457ea48f8c06abca17fd50bc31348eee8374
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-17 16:44:41 +0100

    Update outputs.tf

--- infrastructure/modules/rds/outputs.tf
+++ infrastructure/modules/rds/outputs.tf
@@ -9,3 +9,7 @@ output "rds_cluster_identifier" {
 output "rds_secretsmanager_secret_arn" {
   value = aws_secretsmanager_secret.rds_secret.arn
 }
+
+output "rds_sg_id" {
+  value = aws_security_group.rds_sg.id
+}
