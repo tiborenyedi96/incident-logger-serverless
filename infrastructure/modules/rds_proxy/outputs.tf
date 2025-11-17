cbefe6fd5b8a8540de8993c395300110135444d4
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-17 16:44:43 +0100

    Update outputs.tf

---
+++ infrastructure/modules/rds_proxy/outputs.tf
@@ -0,0 +1,11 @@
+output "rds_proxy_arn" {
+  value = aws_db_proxy.this.arn
+}
+
+output "proxy_endpoint" {
+  value = aws_db_proxy.this.endpoint
+}
+
+output "rds_proxy_security_group_id" {
+  value = aws_security_group.rds_proxy_sg.id
+}
