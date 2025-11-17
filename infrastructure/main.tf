3880545dc95731d535509cf8aa7e93ec9aac2015
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-17 19:19:02 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -44,6 +44,19 @@ module "lambda" {
   rds_proxy_endpoint            = module.rds_proxy.proxy_endpoint
 }
 
+module "vpc_endpoints" {
+  source = "./modules/vpc_endpoints"
+
+  name   = "incident-logger"
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = [
+    module.vpc.private_a_subnet_id,
+    module.vpc.private_b_subnet_id
+  ]
+  aws_region = "eu-central-1"
+}
+
+
 //Security group rule definitions
 resource "aws_security_group_rule" "allow_lambda_to_rds_proxy" {
   type                     = "ingress"
@@ -61,4 +74,40 @@ resource "aws_security_group_rule" "allow_rds_proxy_to_rds" {
   protocol                 = "tcp"
   security_group_id        = module.rds.rds_sg_id
   source_security_group_id = module.rds_proxy.rds_proxy_security_group_id
+}
+
+resource "aws_security_group_rule" "lambda_egress_to_rds_proxy" {
+  type                     = "egress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = module.lambda.lambda_security_group_id
+  source_security_group_id = module.rds_proxy.rds_proxy_security_group_id
+}
+
+resource "aws_security_group_rule" "lambda_https_to_vpce" {
+  type                     = "egress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = module.lambda.lambda_security_group_id
+  source_security_group_id = module.vpc_endpoints.vpce_security_group_id
+}
+
+resource "aws_security_group_rule" "vpce_ingress_from_lambda" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = module.vpc_endpoints.vpce_security_group_id
+  source_security_group_id = module.lambda.lambda_security_group_id
+}
+
+resource "aws_security_group_rule" "rds_proxy_egress_to_rds" {
+  type                     = "egress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = module.rds_proxy.rds_proxy_security_group_id
+  source_security_group_id = module.rds.rds_sg_id
 }
