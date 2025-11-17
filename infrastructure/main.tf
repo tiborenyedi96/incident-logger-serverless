9bbfd25056d588f8930281e5bae4e0a97d9307b9
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-17 16:44:34 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,9 +17,9 @@ module "rds" {
 }
 
 module "rds_proxy" {
-  source          = "./modules/rds_proxy"
-  name            = "incident-logger"
-  vpc_id          = module.vpc.vpc_id
+  source                 = "./modules/rds_proxy"
+  name                   = "incident-logger"
+  vpc_id                 = module.vpc.vpc_id
   rds_cluster_identifier = module.rds.rds_cluster_identifier
 
   subnet_ids = [
@@ -29,4 +29,36 @@ module "rds_proxy" {
 
   rds_cluster_arn               = module.rds.rds_cluster_arn
   rds_secretsmanager_secret_arn = module.rds.rds_secretsmanager_secret_arn
+}
+
+module "lambda" {
+  source = "./modules/lambda"
+  name   = "incident-logger"
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = [
+    module.vpc.private_a_subnet_id,
+    module.vpc.private_b_subnet_id
+  ]
+
+  rds_secretsmanager_secret_arn = module.rds.rds_secretsmanager_secret_arn
+  rds_proxy_endpoint            = module.rds_proxy.proxy_endpoint
+}
+
+//Security group rule definitions
+resource "aws_security_group_rule" "allow_lambda_to_rds_proxy" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = module.rds_proxy.rds_proxy_security_group_id
+  source_security_group_id = module.lambda.lambda_security_group_id
+}
+
+resource "aws_security_group_rule" "allow_rds_proxy_to_rds" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = module.rds.rds_sg_id
+  source_security_group_id = module.rds_proxy.rds_proxy_security_group_id
 }
