9f9588a6129f1747be2d3335daed654c61e139a5
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-19 17:06:12 +0100

    Update bootstrap.tf

--- bootstrap/bootstrap.tf
+++ bootstrap/bootstrap.tf
@@ -19,6 +19,22 @@ resource "aws_s3_bucket" "tf_state" {
   force_destroy = false
 }
 
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "tf_state_block_public_access" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_dynamodb_table" "tf_lock" {
   name         = "incident-logger-tf-lock"
   billing_mode = "PAY_PER_REQUEST"
