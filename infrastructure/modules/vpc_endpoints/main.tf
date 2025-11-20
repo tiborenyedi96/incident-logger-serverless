6ba305b78f7a93512fd380d97059c054e8d39944
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-20 17:34:22 +0100

    Update main.tf

--- infrastructure/modules/vpc_endpoints/main.tf
+++ infrastructure/modules/vpc_endpoints/main.tf
@@ -4,21 +4,21 @@ resource "aws_security_group" "vpce_sg" {
   vpc_id      = var.vpc_id
 
   tags = {
-    Name = "${var.name}-vpce-sg"
+    Name    = "${var.name}-vpce-sg"
     Project = var.name
   }
 }
 
 resource "aws_vpc_endpoint" "secretsmanager" {
-  vpc_id             = var.vpc_id
-  service_name       = "com.amazonaws.${var.aws_region}.secretsmanager"
-  vpc_endpoint_type  = "Interface"
-  subnet_ids         = var.subnet_ids
-  security_group_ids = [aws_security_group.vpce_sg.id]
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.secretsmanager"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = var.subnet_ids
+  security_group_ids  = [aws_security_group.vpce_sg.id]
   private_dns_enabled = true
 
   tags = {
-    Name = "${var.name}-secretsmanager-endpoint"
+    Name    = "${var.name}-secretsmanager-endpoint"
     Project = var.name
   }
 }
