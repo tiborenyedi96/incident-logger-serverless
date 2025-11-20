250800a9dc153547ef36f177f8ca2c23fa523037
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-20 17:35:03 +0100

    Refactored security group rules

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -56,8 +56,29 @@ module "vpc_endpoints" {
   aws_region = "eu-central-1"
 }
 
+//Egress SG rules
+locals {
+  project_sg_ids = [
+    module.lambda.lambda_security_group_id,
+    module.rds_proxy.rds_proxy_security_group_id,
+    module.rds.rds_sg_id,
+    module.vpc_endpoints.vpce_security_group_id
+  ]
+}
 
-//SG rules
+resource "aws_security_group_rule" "egress_within_vpc" {
+  for_each = toset(local.project_sg_ids)
+
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "all"
+  cidr_blocks       = [module.vpc.cidr_block]
+  security_group_id = each.key
+  description       = "Allows egress traffic within VPC (${module.vpc.vpc_id})"
+}
+
+//Ingress SG rules
 resource "aws_security_group_rule" "rds_proxy_ingress_from_lambda" {
   type                     = "ingress"
   from_port                = 3306
@@ -76,24 +97,6 @@ resource "aws_security_group_rule" "rds_ingress_from_rds_proxy" {
   source_security_group_id = module.rds_proxy.rds_proxy_security_group_id
 }
 
-resource "aws_security_group_rule" "lambda_egress_to_rds_proxy" {
-  type                     = "egress"
-  from_port                = 3306
-  to_port                  = 3306
-  protocol                 = "tcp"
-  security_group_id        = module.lambda.lambda_security_group_id
-  source_security_group_id = module.rds_proxy.rds_proxy_security_group_id
-}
-
-resource "aws_security_group_rule" "lambda_egress_to_vpce" {
-  type                     = "egress"
-  from_port                = 443
-  to_port                  = 443
-  protocol                 = "tcp"
-  security_group_id        = module.lambda.lambda_security_group_id
-  source_security_group_id = module.vpc_endpoints.vpce_security_group_id
-}
-
 resource "aws_security_group_rule" "vpce_ingress_from_lambda" {
   type                     = "ingress"
   from_port                = 443
@@ -101,13 +104,4 @@ resource "aws_security_group_rule" "vpce_ingress_from_lambda" {
   protocol                 = "tcp"
   security_group_id        = module.vpc_endpoints.vpce_security_group_id
   source_security_group_id = module.lambda.lambda_security_group_id
-}
-
-resource "aws_security_group_rule" "rds_proxy_egress_to_rds" {
-  type                     = "egress"
-  from_port                = 3306
-  to_port                  = 3306
-  protocol                 = "tcp"
-  security_group_id        = module.rds_proxy.rds_proxy_security_group_id
-  source_security_group_id = module.rds.rds_sg_id
 }
