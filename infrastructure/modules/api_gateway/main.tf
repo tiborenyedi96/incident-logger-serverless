03576e6211625d349d0a9ffe0af793b1ab3c4a04
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-23 14:42:44 +0100

    Created the API gateway module

---
+++ infrastructure/modules/api_gateway/main.tf
@@ -0,0 +1,27 @@
+resource "aws_apigatewayv2_api" "this" {
+  name          = "${var.name}-API-GW"
+  protocol_type = "HTTP"
+  body          = file("${path.module}/openapi-definition.yaml")
+}
+
+resource "aws_lambda_permission" "allow_apigw_get" {
+  statement_id  = "AllowAPIInvokeGET"
+  action        = "lambda:InvokeFunction"
+  function_name = var.get_function_arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.this.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "allow_apigw_post" {
+  statement_id  = "AllowAPIInvokePOST"
+  action        = "lambda:InvokeFunction"
+  function_name = var.post_function_arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.this.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.this.id
+  name        = "$default"
+  auto_deploy = true
+}
---
+++ infrastructure/modules/api_gateway/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  type = string
+}
+
+variable "get_function_invoke_arn" {
+  type = string
+}
+
+variable "post_function_invoke_arn" {
+  type = string
+}
+
+variable "get_function_arn" {
+  type = string
+}
+
+variable "post_function_arn" {
+  type = string
+}
