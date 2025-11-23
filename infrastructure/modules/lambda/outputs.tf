c37d3b0e2259fb43b083ea134682b096edf43d0e
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-23 14:42:22 +0100

    Update outputs.tf

--- infrastructure/modules/lambda/outputs.tf
+++ infrastructure/modules/lambda/outputs.tf
@@ -1,3 +1,19 @@
 output "lambda_security_group_id" {
   value = aws_security_group.lambda_sg.id
 }
+
+output "get_function_invoke_arn" {
+  value = aws_lambda_function.lambda_get_incidents.invoke_arn
+}
+
+output "post_function_invoke_arn" {
+  value = aws_lambda_function.lambda_post_incident.invoke_arn
+}
+
+output "get_function_arn" {
+  value = aws_lambda_function.lambda_get_incidents.arn
+}
+
+output "post_function_arn" {
+  value = aws_lambda_function.lambda_post_incident.arn
+}
