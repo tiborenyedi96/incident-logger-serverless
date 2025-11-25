18fbe985db29c410819d6da21d38665485071f55
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-25 21:15:47 +0100

    More IAM policy fix for pipeline

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -101,6 +101,7 @@ resource "aws_iam_policy" "github_actions_terraform_policy" {
           "s3:GetBucketLocation",
           "s3:GetBucketCORS",
           "s3:GetBucketWebsite",
+          "s3:GetBucketVersioning",
           "cloudfront:Get*",
           "cloudfront:List*"
         ]
