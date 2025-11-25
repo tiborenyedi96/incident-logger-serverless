14a8d5112dfd535519bd9686530642852016a2cb
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-25 21:21:08 +0100

    Update main.tf

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -103,6 +103,7 @@ resource "aws_iam_policy" "github_actions_terraform_policy" {
           "s3:GetBucketWebsite",
           "s3:GetBucketVersioning",
           "s3:GetAccelerateConfiguration",
+          "s3:GetBucketRequestPayment",
           "cloudfront:Get*",
           "cloudfront:List*"
         ]
