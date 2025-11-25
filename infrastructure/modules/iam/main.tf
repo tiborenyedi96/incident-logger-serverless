ff122441245b373a288f82cae66f8d0e87a55bc2
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-25 21:35:22 +0100

    Update main.tf

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -15,17 +15,17 @@ resource "aws_iam_role" "github_actions_terraform_role" {
   name = "${var.name}-github-actions-terraform-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Principal = {
           Federated = aws_iam_openid_connect_provider.github_actions_oidc.arn
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
           StringEquals = {
-            "token.actions.githubusercontent.com:sub" = "repo:tiborenyedi96/incident-logger-serverless:pull_request"
+            "token.actions.githubusercontent.com:sub" = "repo:tiborenyedi96/incident-logger-serverless:pull_request",
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
         }
@@ -39,45 +39,55 @@ resource "aws_iam_policy" "github_actions_terraform_policy" {
   description = "Terraform PLAN policy for GitHub Actions"
 
   policy = jsonencode({
-    Version : "2012-10-17"
-    Statement : [
-
+    Version = "2012-10-17",
+    Statement = [
       {
-        Sid : "TFStateS3Access"
-        Effect : "Allow"
+        Sid : "TFStateS3Access",
+        Effect : "Allow",
         Action : [
           "s3:GetObject",
           "s3:PutObject",
           "s3:DeleteObject",
           "s3:ListBucket"
-        ]
+        ],
         Resource : [
           "arn:aws:s3:::incident-logger-tf-state",
           "arn:aws:s3:::incident-logger-tf-state/*"
         ]
       },
       {
-        Sid : "FrontendBucketListAccess"
-        Effect : "Allow"
+        Sid : "FrontendBucketListOnly",
+        Effect : "Allow",
         Action : [
           "s3:ListBucket"
-        ]
+        ],
         Resource : "arn:aws:s3:::incident-logger-frontend"
       },
       {
-        Sid : "TFStateDynamoDBLockAccess"
-        Effect : "Allow"
+        Sid : "FrontendBucketMetadata",
+        Effect : "Allow",
+        Action : [
+          "s3:GetBucket*",
+          "s3:GetAccelerateConfiguration",
+          "s3:GetEncryptionConfiguration",
+          "s3:GetIntelligentTieringConfiguration"
+        ],
+        Resource : "arn:aws:s3:::incident-logger-frontend"
+      },
+      {
+        Sid : "TFStateDynamoDBLockAccess",
+        Effect : "Allow",
         Action : [
           "dynamodb:GetItem",
           "dynamodb:PutItem",
           "dynamodb:DeleteItem",
           "dynamodb:DescribeTable"
-        ]
+        ],
         Resource : "arn:aws:dynamodb:eu-central-1:299097238534:table/incident-logger-tf-lock"
       },
       {
-        Sid : "ReadOnlyForPlan"
-        Effect : "Allow"
+        Sid : "ReadOnlyForPlan",
+        Effect : "Allow",
         Action : [
           "ec2:Describe*",
           "rds:Describe*",
@@ -94,15 +104,10 @@ resource "aws_iam_policy" "github_actions_terraform_policy" {
           "secretsmanager:GetResourcePolicy",
           "secretsmanager:ListSecrets",
           "secretsmanager:DescribeSecret",
-          "cloudfront:Get*",
-          "cloudfront:List*",
-          "s3:GetBucket*",
-          "s3:GetObject*",
-          "s3:ListBucket",
           "s3:ListAllMyBuckets",
-          "s3:GetEncryptionConfiguration",
-          "s3:GetIntelligentTieringConfiguration"
-        ]
+          "cloudfront:Get*",
+          "cloudfront:List*"
+        ],
         Resource : "*"
       }
     ]
