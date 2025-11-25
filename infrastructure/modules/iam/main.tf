64fff106819b0aef5f4fa67641647d1d0928a0e4
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-25 15:25:01 +0100

    Update main.tf

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -7,3 +7,47 @@ resource "aws_iam_openid_connect_provider" "github_actions_oidc" {
 
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
 }
+
+resource "aws_iam_role" "github_actions_terraform_role" {
+  name = "${var.name}-github-actions-terraform-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions_oidc.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:sub" : "repo:tiborenyedi96/incident-logger-serverless:pull_request",
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "github_actions_terraform_policy" {
+  name        = "${var.name}-github-actions-terraform-policy"
+  description = "Policy for Github actions access"
+
+  policy = jsonencode({
+    Version : "2012-10-17",
+    Statement : [
+      {
+        Effect : "Allow",
+        Action : "*",
+        Resource : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_terraform_policy_attachment" {
+  role       = aws_iam_role.github_actions_terraform_role.name
+  policy_arn = aws_iam_policy.github_actions_terraform_policy.arn
+}
