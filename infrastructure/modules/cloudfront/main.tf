36247b17f9052970d83e2fe223324b9efb447778
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-11-28 21:42:47 +0100

    Update main.tf

--- infrastructure/modules/cloudfront/main.tf
+++ infrastructure/modules/cloudfront/main.tf
@@ -50,6 +50,15 @@ resource "aws_cloudfront_distribution" "this" {
 
     allowed_methods = ["GET", "HEAD"]
     cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
   }
 
   viewer_certificate {
