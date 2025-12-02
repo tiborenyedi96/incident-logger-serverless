f8fddf22fe321fa2574ef8c9bc4a83ce4e75045b
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-12-02 15:33:58 +0100

    Update main.tf

--- infrastructure/modules/ecr/main.tf
+++ infrastructure/modules/ecr/main.tf
@@ -1,6 +1,11 @@
 resource "aws_ecr_repository" "get_repository" {
   name                 = "${var.name}-lambda-get-image-repository"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
+
+  image_tag_mutability_exclusion_filter {
+    filter      = "latest*"
+    filter_type = "WILDCARD"
+  }
 
   encryption_configuration {
     encryption_type = "AES256"
@@ -9,7 +14,12 @@ resource "aws_ecr_repository" "get_repository" {
 
 resource "aws_ecr_repository" "post_repository" {
   name                 = "${var.name}-lambda-post-image-repository"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
+
+  image_tag_mutability_exclusion_filter {
+    filter      = "latest*"
+    filter_type = "WILDCARD"
+  }
 
   encryption_configuration {
     encryption_type = "AES256"
