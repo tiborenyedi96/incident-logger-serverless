a0e326f34b9edd44c7f34499e2b58033e33878c5
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-12-02 19:38:17 +0100

    Still trying to fix the IAM issue/Added repository policies

--- infrastructure/modules/ecr/main.tf
+++ infrastructure/modules/ecr/main.tf
@@ -25,3 +25,45 @@ resource "aws_ecr_repository" "post_repository" {
     encryption_type = "AES256"
   }
 }
+
+resource "aws_ecr_repository_policy" "get_repository_policy" {
+  repository = aws_ecr_repository.get_repository.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "LambdaECRImageRetrievalPolicy"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_ecr_repository_policy" "post_repository_policy" {
+  repository = aws_ecr_repository.post_repository.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "LambdaECRImageRetrievalPolicy"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer"
+        ]
+      }
+    ]
+  })
+}
