62164a73eb1118a7d1d08cd4bfe2aa5a7b29b492
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-12-02 15:20:27 +0100

    Update main.tf

--- infrastructure/modules/lambda/main.tf
+++ infrastructure/modules/lambda/main.tf
@@ -48,11 +48,10 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
 
 //GET function
 resource "aws_lambda_function" "lambda_get_incidents" {
-  filename      = "${path.module}/get-function.zip"
   function_name = "${var.name}-lambda-get-incidents"
+  package_type  = "Image"
+  image_uri     = "${var.repository_url}:latest"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "index.lambda_handler"
-  runtime       = "python3.12"
   memory_size   = var.function_memory
   timeout       = var.function_timeout
 
