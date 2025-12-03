ca42b4673fb02bf9767880a09268bd7cf37b4147
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-12-03 19:51:56 +0100

    Update output.tf

--- infrastructure/modules/cloudfront/output.tf
+++ infrastructure/modules/cloudfront/output.tf
@@ -1,3 +1,11 @@
 output "cloudfront_domain" {
   value = aws_cloudfront_distribution.this.domain_name
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.this.id
+}
+
+output "cloudfront_distribution_arn" {
+  value = aws_cloudfront_distribution.this.arn
+}
