1fb8758cc7576d01f94be62d5e8dc87c7cba37ef
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-12-06 13:44:10 +0100

    Update main.tf

--- infrastructure/modules/vpc/main.tf
+++ infrastructure/modules/vpc/main.tf
@@ -1,3 +1,7 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "this" {
   cidr_block           = var.cidr_block
   enable_dns_support   = true
@@ -7,14 +11,14 @@ resource "aws_vpc" "this" {
 resource "aws_subnet" "private_a" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.private_a
-  availability_zone       = "eu-central-1a"
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = false
 }
 
 resource "aws_subnet" "private_b" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.private_b
-  availability_zone       = "eu-central-1b"
+  availability_zone       = data.aws_availability_zones.available.names[1]
   map_public_ip_on_launch = false
 }
 
