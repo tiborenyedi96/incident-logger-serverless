1b33382734f486d0169e557e448929bdd2942928
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-12-08 11:20:15 +0100

    Update main.tf

--- infrastructure/modules/monitoring/main.tf
+++ infrastructure/modules/monitoring/main.tf
@@ -1,30 +1,7 @@
 resource "aws_cloudwatch_dashboard" "main" {
-  dashboard_name = "my-dashboard"
+  dashboard_name = "incident-logger-lambda-dashboard"
 
-  dashboard_body = jsonencode({
-    widgets = [
-      {
-        type   = "metric"
-        x      = 0
-        y      = 0
-        width  = 12
-        height = 12
+  dashboard_body = templatefile("${path.module}/lambda-dashboard.tpl.json", {
 
-        properties = {
-          metrics = [
-            [
-              "AWS/Lambda",
-              "Errors",
-              "FunctionName",
-              "incident-logger-lambda-get-incidents"
-            ]
-          ]
-          period = 300
-          stat   = "Average"
-          region = "eu-central-1"
-          title  = "Lambda GET errors"
-        }
-      }
-    ]
   })
 }
