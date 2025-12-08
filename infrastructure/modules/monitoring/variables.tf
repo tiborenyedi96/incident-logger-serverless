4d39beb193ad62398f569755d86b5a235de0bb78
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-12-08 18:06:42 +0100

    Update variables.tf

--- infrastructure/modules/monitoring/variables.tf
+++ infrastructure/modules/monitoring/variables.tf
@@ -2,3 +2,27 @@ variable "alarm_email" {
   type      = string
   sensitive = true
 }
+
+variable "lambda_get_function_name" {
+  type        = string
+  description = "Name of the GET Lambda function"
+  default     = "incident-logger-lambda-get-incidents"
+}
+
+variable "lambda_post_function_name" {
+  type        = string
+  description = "Name of the POST Lambda function"
+  default     = "incident-logger-lambda-post-incident"
+}
+
+variable "api_gateway_name" {
+  type        = string
+  description = "Name of the API Gateway"
+  default     = "incident-logger-API-GW"
+}
+
+variable "rds_cluster_identifier" {
+  type        = string
+  description = "RDS cluster identifier"
+  default     = "incident-logger-rds-cluster"
+}
