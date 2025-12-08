11b0bff957d0cf6a3213565e2bce9c43bf99f17b
tiborenyedi96 <tibor.enyedi1996@gmail.com>
2025-12-08 11:41:14 +0100

    Update main.tf

--- infrastructure/modules/monitoring/main.tf
+++ infrastructure/modules/monitoring/main.tf
@@ -1,7 +1,9 @@
-resource "aws_cloudwatch_dashboard" "main" {
+resource "aws_cloudwatch_dashboard" "lambda_dashboard" {
   dashboard_name = "incident-logger-lambda-dashboard"
+  dashboard_body = templatefile("${path.module}/lambda-dashboard.tpl.json", {})
+}
 
-  dashboard_body = templatefile("${path.module}/lambda-dashboard.tpl.json", {
-
-  })
+resource "aws_cloudwatch_dashboard" "rds_proxy_dashboard" {
+  dashboard_name = "incident-logger-rds-proxy-dashboard"
+  dashboard_body = templatefile("${path.module}/rdsproxy-dashboard.tpl.json", {})
 }
